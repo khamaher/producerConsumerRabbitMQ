bcab0f3972bb3ddf798b162ca74cd0890a5e689f
khaled maher <khaled.maher@gmail.com>
2020-05-29 18:26:13 +0200

    first Commit

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("basicTest", false, false, false, null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                  {
+                      var body = ea.Body.ToArray();
+                      var message = Encoding.UTF8.GetString(body);
+                      Console.WriteLine("Receive message {0}", message);
+                  };
+                channel.BasicConsume("basicTest", true, consumer);
+
+            }
+            Console.WriteLine("press any key to exist");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName="localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("basicTest", false, false, false, null);
+                string message = "Getting started with .net core rabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "basicTest", null, body);
+                Console.WriteLine("send message  {0}", message);
+            
+            }
+            Console.WriteLine("press any key to exist");
+        }
+    }
+}
